cc811df8607e305166ad8f50fd016207b07332d2
anthonyolds <anthonyolds@gmail.com>
2015-03-24 13:27:57 -0700

    Fixed Matches List and added some extra data

--- app/controllers/MatchController.js
+++ app/controllers/MatchController.js
@@ -6,6 +6,9 @@ var MatchController = function($scope, $http, $location, lolapi, $routeParams) {
 	
 	var sumMatchRecieved = function(response){
 		$scope.match= response;
+		for (i = 0; i < $scope.match.participantIdentities.length; i++){
+			$scope.match.participants[i].summonerName = $scope.match.participantIdentities[i].player.summonerName;
+		}
 	};
 
 	var onError = function(reason){
