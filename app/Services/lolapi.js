4437b006f0df00f758f2e13c34e1aa0d17d57b79
Gai <gail@SEAGAIL0314.local>
2015-06-11 02:58:11 -0700

    cleanup

--- app/Services/lolapi.js
+++ app/Services/lolapi.js
@@ -1,52 +1,48 @@
-(function() {
+var app = angular.module("LeagueViewer");
 
-  var lolapi = function($http) {
-    var apiKey = "1a417329-8a55-43cb-9262-928bff0ccec9";
+app.factory("lolapi", function($http) {
+  var apiKey = "1a417329-8a55-43cb-9262-928bff0ccec9";
+  var baseUrl = "https://na.api.pvp.net/api/lol/";
 
-    var getSummoner = function(summonerName) {
-      return $http.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey)
-        .then(function(response) {
-          return response.data;
-        });
-    };
-
-    var getMatchHistory = function(summonerId){
-      return $http.get("https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/" + summonerId + "?api_key=" + apiKey)
-        .then(function(response){
-          return response.data;
-        });
-    };
-
-    var getMatch = function(matchId){
-        return $http.get("https://na.api.pvp.net/api/lol/na/v2.2/match/" + matchId + "?api_key=" + apiKey)
-          .then(function(response){
-            return response.data;
-          });
-    };
-
-    var getChamp = function(championId){
-        return $http.get("https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + championId + "?api_key=" + apiKey)
-          .then(function(response){
-            return response.data;
-        });
-    };
-
-    var getRankedStats = function(championId) {
-        return $http.get("https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + championId + "/ranked?api_key=" + apiKey)
-          .then(function(response){
-            return response.data;
-        });
-    };
-
-    return {
-      getSummoner : getSummoner,
-      getMatchHistory : getMatchHistory,
-      getMatch : getMatch,
-      getChamp : getChamp
-    };
+  var getSummoner = function(summonerName) {
+    return $http.get(baseUrl + "na/v1.4/summoner/by-name/" + summonerName + "?api_key=" + apiKey)
+      .then(function(response) {
+        return response.data;
+      });
   };
 
-  var module = angular.module("LeagueViewer");
-  module.factory("lolapi", lolapi);
-  
-}());
+  var getMatchHistory = function(summonerId) {
+    return $http.get(baseUrl + "na/v2.2/matchhistory/" + summonerId + "?api_key=" + apiKey)
+      .then(function(response) {
+        return response.data;
+      });
+  };
+
+  var getMatch = function(matchId) {
+    return $http.get(baseUrl + "na/v2.2/match/" + matchId + "?api_key=" + apiKey)
+      .then(function(response) {
+        return response.data;
+      });
+  };
+
+  var getChamp = function(championId) {
+    return $http.get(baseUrl + "static-data/na/v1.2/champion/" + championId + "?api_key=" + apiKey)
+      .then(function(response) {
+        return response.data;
+    });
+  };
+
+  var getRankedStats = function(championId) {
+    return $http.get(baseUrl + "na/v1.3/stats/by-summoner/" + championId + "/ranked?api_key=" + apiKey)
+      .then(function(response) {
+        return response.data;
+    });
+  };
+
+  return {
+    getSummoner : getSummoner,
+    getMatchHistory : getMatchHistory,
+    getMatch : getMatch,
+    getChamp : getChamp
+  };
+});
---
+++ app/match/match-ctrl.js
@@ -0,0 +1,24 @@
+angular.module("LeagueViewer")
+	.controller('MatchController',
+	[
+		'$scope',
+		'$http',
+		'$location',
+		'lolapi',
+		'$routeParams',		
+		function($scope, $http, $location, lolapi, $routeParams) {
+			var summonerMatchRecieved = function(matchInfo) {
+				$scope.match = matchInfo;
+				for (var i = 0; i < $scope.match.participantIdentities.length; i++) {
+					$scope.match.participants[i].summonerName = $scope.match.participantIdentities[i].player.summonerName;
+				}
+			};
+
+			var onError = function(error) {
+				$scope.error = "Could not fetch match information";
+			};
+
+			lolapi.getMatch($routeParams.matchId).then(summonerMatchRecieved, onError);
+		}
+	]
+);
